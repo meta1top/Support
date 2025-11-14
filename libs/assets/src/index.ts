89c161057324d4ca4343d9c265925c654e98de7a
Grant <284885069@qq.com>
2025-11-15 02:10:48 +0800

    feat: export dto

--- libs/assets/src/index.ts
+++ libs/assets/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./assets.module";
 export * from "./assets.service";
 export * from "./config";
+export * from "./dto";
 export * from "./oss";
 export * from "./s3";
 export * from "./shared";
